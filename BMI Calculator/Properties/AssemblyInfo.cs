458c1d4f4a64fe87a47336eb8e2c1d809e51087d
K <ca11iditas700@gmail.com>
2019-08-05 22:40:29 -0500

    Recommit

--- BMI Calculator/Properties/AssemblyInfo.cs
+++ BMI Calculator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BMI Caliculator")]
+[assembly: AssemblyTitle("BMI Calculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BMI Caliculator")]
+[assembly: AssemblyProduct("BMI Calculator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
